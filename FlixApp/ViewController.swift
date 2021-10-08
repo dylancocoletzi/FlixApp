e3323b161fd4aa41e69a6cecdb331c9827a0c8b8
dylancocoletzi <86747062+dylancocoletzi@users.noreply.github.com>
2021-10-08 14:50:22 -0700

    Initial Commit

---
+++ FlixApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlixApp
+//
+//  Created by Dylan Cocoletzi on 10/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
